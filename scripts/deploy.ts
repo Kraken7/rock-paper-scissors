2fc4742e74c76c5ea3ae069d1765cfbc98f26bf1
aleksey <aleksey.saraev.97@mail.ru>
2023-02-28 21:43:18 +0500

    contract is written

---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat";
+import { RockPaperScissors, RockPaperScissors__factory } from "../typechain-types";
+
+async function main() {
+  const RockPaperScissors: RockPaperScissors__factory = await ethers.getContractFactory("RockPaperScissors");
+  const rockPaperScissors: RockPaperScissors = await RockPaperScissors.deploy();
+  await rockPaperScissors.deployed();
+
+  console.log(rockPaperScissors.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
